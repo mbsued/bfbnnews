6cad1df8e5762ee424bd71152b501e2c30dfeecc
mb-west <hans-peter.schloegl@bfbn.de>
2025-11-10 10:18:16 +0100

    v13.0.0
    Update auf Typo3 Version 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,11 +19,11 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.0',
+    'version' => '13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-12.99.99',
-			'news' => '9.0.0-11.99.99',
+            'typo3' => '11.0.0-13.99.99',
+			'news' => '9.0.0-13.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
